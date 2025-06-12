2a9e3709160b50956a2d5ea41585fcd25e2923a0
Jun Xu <63337934+gekjunxu@users.noreply.github.com>
2025-06-12 23:25:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar
+java -jar RepoSense.jar --repos https://github.com/gekjunxu/ip.git --since 1/1/2025
